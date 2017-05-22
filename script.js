40f1046dd43093b1cf90f846850f6c3869b90150
Jason <alvarezcoraiza@citypolyhs.org>
2017-05-22 20:30:22 +0000

    dammit barry

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 /*global $*/
-$(document).ready(function () {
-    window.setInterval(function(){
-        var width = Math.random();
-        var yeniwidth = width * 500;
-        margin = Math.round(yeniwidth);
-        $("#circle").css("margin-top", margin + "px");
-        var height = Math.random();
-        var yeniheight = height * 1000;
-        margin2 = Math.round(yeniheight);
-        $("#circle").css("margin-left", margin2 + "px");
-    }, 5000);
+$(document).ready(function() {
+    setInterval(function(){
+        console.log("1 second");
+        fall();
+    },
+    1000);
+    //just change the number 5000 to adjust the time that should pass before the code is executed again (1000ms is 1 second).
+
+    function fall(){
+        $(".enemy").css("top", $(".enemy").offset().top + 10);
+    };
+    
+    function collission(){
+        var enemyHeight = $(".enemy").offset().top + $("#paddle").height();
+        var enemyWidth = $(".enemy").offset().left + $("#paddle").width();
+        var heroHieght = $("#paddle").offset().top + $("#paddle").height();
+        var heroWidth = $("#paddle").offset().left + $("#paddle").width();
+        if(enemyHeight > heroHieght && enemyWidth ) {
+            $(".enemy").offset().top - 460;
+        }
+    };
+
+
+
 });
-//just change the number 5000 to adjust the time that should pass before the code is executed again (1000ms is 1 second).
+
+$("body").keydown(function(event) {
+    console.log(event)
+    if (event.keyCode ===37) {
+        $("#paddle").css("left", $("#paddle").offset().left- 1);
+    } else if (event.keyCode ===39){
+        $("#paddle").css("left", $("#paddle").offset().left + 1);
+    } else{
+        return;
+    }
+});
+
+
 
